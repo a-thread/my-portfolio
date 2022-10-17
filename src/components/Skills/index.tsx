ed72b64fda72b0e9ef9943bb88b1e1476d06dc02
a-thread <aiden.threadgoode@gmail.com>
2022-10-16 20:43:08 -0400

    updating skills

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -11,7 +11,7 @@ export default class Skills extends Component {
                     <div>
                         <span><i className='fas fa-laptop-code'></i></span>
                         <h4>Front End</h4>
-                        <p>Typescript, Angular, React, Bootstrap, Material UI, rxjs, ngrx, redux, HTML, CSS, SASS jQuery, Bootstrap, Material UI</p>
+                        <p>Typescript, Angular, React, Bootstrap, Material UI, rxjs, ngrx, redux, HTML, SASS</p>
                     </div>
                     <div>
                         <span><i className='fas fa-terminal'></i></span>
@@ -23,7 +23,7 @@ export default class Skills extends Component {
                     <div>
                         <span className='text-primary-light'><i className='fas fa-code-branch'></i></span>
                         <h4>Tools</h4>
-                        <p>Git, Github, Jira, , Hubspot, Slack, Adobe Creative Suite</p>
+                        <p>Git, Github, Jira, Adobe Creative Suite</p>
                     </div>
                     <div className='soft'>
                         <span className='text-primary-light'><i className='fas fa-users-cog'></i></span>
