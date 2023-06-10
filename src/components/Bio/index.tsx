037ffb62bce1fc81770e95deaa6d8244722d62a6
a-thread <aiden.threadgoode@gmail.com>
2023-06-10 08:32:15 -0400

    updating bio

--- src/components/Bio/index.tsx
+++ src/components/Bio/index.tsx
@@ -17,10 +17,6 @@ export default class Bio extends Component {
                         Coming from the nonprofit sector with a focus upon peer support and social justice, I am
                         passionate about accessibility and approach programming challenges from fresh perspectives 
                         to create collaborative and meaningful web applications.
-                        <br />
-                        <br />
-                        When I'm not hard at work, you can find me making art (quilting, drawing, or refinishing furniture) with my spouse,
-                        CJ, and great dane, Bonito. He doesn't have thumbs but is always willing to throw a few barks in!
                         </p>
                 </article>
             </div>
