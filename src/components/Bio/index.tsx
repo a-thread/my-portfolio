98d88c2ff29a86881e56b5d148df2cb1ccc60125
Aiden Threadgoode <a.threadless@proton.me>
2025-03-03 09:27:48 -0500

    updating intro

--- src/components/Bio/index.tsx
+++ src/components/Bio/index.tsx
@@ -8,7 +8,7 @@ const Bio: React.FC = () => {
 
             <article>
                 <p className='bio-content'>
-                    Full Stack Engineer with 3+ years of hands on experience designing, building, and implementing applications
+                    Full Stack Engineer with 4+ years of hands on experience designing, building, and implementing applications
                     using a wide range of technologies and programming languages. Check out my projects page to see a few of my
                     personal projects!
                     <br />
