206fc48eedc6ba2dd434a45e7c59549ca7e38e9c
Aiden Threadgoode <a.threadless@proton.me>
2026-08-23 12:26:48 -0400

    first pass at new theme and headshot

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -1,62 +1,59 @@
-import React, { useState } from "react";
-import { Link } from 'react-router-dom';
-import './style.scss';
-import { Button } from 'react-bootstrap'
-import Typewriter from 'typewriter-effect';
-import { useDarkMode } from "@shared/state/DarkModeContext";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.scss";
+import { Button } from "react-bootstrap";
+import Typewriter from "typewriter-effect";
 
 const Intro: React.FC = () => {
-    const { isDarkMode } = useDarkMode();
-    const [imgFailed, setImgFailed] = useState(false);
-
-    const colorPhoto = "/images/aiden.png";
-    const contrastPhoto = "/images/headshot.png";
-
-    const reloadSrc = () => {
-        // If the theme-matched photo fails to load, fall back once to the
-        // other photo rather than looping or showing a mismatched theme.
-        if (!imgFailed) setImgFailed(true);
-    };
-
-    const activeSrc = imgFailed
-        ? (isDarkMode ? colorPhoto : contrastPhoto)
-        : (isDarkMode ? contrastPhoto : colorPhoto);
-
-    return (
-        <div className='intro bg-accent w-100 h-100 d-flex flex-column justify-content-center'>
-            <div className='d-flex flex-wrap align-items-center justify-content-center main-container'>
-                <div className='headshot-container'>
-                    {/* image */}
-                    <img src={activeSrc} onError={reloadSrc} className='img-fluid animate__animated animate__rollIn custom-shadow' alt='Headshot of Aiden' />
-                </div>
-                <div className='text-container'>
-                    {/* Intro title */}
-                    <h1 className='text-secondary-light intro-title animate__animated animate__backInLeft'>
-                        Hi, I'm <span className='text-primary-light'>Aiden.</span>
-                    </h1>
-                    {/* Typewriter effect*/}
-                    <h2 className='type-container text-secondary-light mt-3'>
-                        <Typewriter
-                            options={{
-                                strings: ['Senior Software Engineer', 'Angular & .NET / AWS', 'Building for Scale'],
-                                autoStart: true,
-                                loop: true,
-                            }}
-                        />
-                    </h2>
-                    {/* Project button */}
-                    <Link to='/projects'>
-                        <Button variant='flat' className='mt-3 custom-shadow projects animate__animated animate__backInRight'> recent projects </Button>
-                    </Link>
-                </div>
-            </div>
-            {/* Bouncing arrow */}
-            <a href='#bio' aria-label='link to bio area'>
-                <div className='bg-accent arrow text-white text-center w-100 mt-10'>
-                    <i className='fas fa-chevron-down' />
-                </div>
-            </a>
+  return (
+    <div className="intro bg-accent w-100 h-100 d-flex flex-column justify-content-center">
+      <div className="d-flex flex-wrap align-items-center justify-content-center main-container">
+        <div className="headshot-container">
+          {/* image */}
+          <img
+            src="/images/headshot.png"
+            className="img-fluid animate__animated animate__rollIn custom-shadow"
+            alt="Headshot of Aiden"
+          />
         </div>
-    )
-}
+        <div className="text-container">
+          {/* Intro title */}
+          <h1 className="text-secondary-light intro-title animate__animated animate__backInLeft">
+            Hi, I'm <span className="text-primary-light">Aiden.</span>
+          </h1>
+          {/* Typewriter effect*/}
+          <h2 className="type-container text-secondary-light mt-3">
+            <Typewriter
+              options={{
+                strings: [
+                  "Senior Software Engineer",
+                  "Angular & .NET / AWS",
+                  "Building for Scale",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h2>
+          {/* Project button */}
+          <Link to="/projects">
+            <Button
+              variant="flat"
+              className="mt-3 custom-shadow projects animate__animated animate__backInRight"
+            >
+              {" "}
+              recent projects{" "}
+            </Button>
+          </Link>
+        </div>
+      </div>
+      {/* Bouncing arrow */}
+      <a href="#bio" aria-label="link to bio area">
+        <div className="bg-accent arrow text-white text-center w-100 mt-10">
+          <i className="fas fa-chevron-down" />
+        </div>
+      </a>
+    </div>
+  );
+};
 export default Intro;
