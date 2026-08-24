0480231ed56b4f6d8a170ae6abaeba06e3f22c31
Aiden Threadgoode <a.threadless@proton.me>
2026-08-23 23:37:43 -0400

    refactor: re-organizing testimonials

--- src/shared/data/testimonials.ts
+++ src/shared/data/testimonials.ts
@@ -1,5 +1,5 @@
 export const testimonials: string[] = [
   "He is always on the lookout for someone not feeling good about themselves — looking out for the dignity of every person on the team.",
-  "It's been an honor having Aiden on our engineering team. From day one, he's demonstrated a strong sense of proactivity, ownership, and initiative.",
   "Aiden's mindset of continuous improvement shows up in everything he does — he's constantly learning new tech, improving the UX, and making the codebase more maintainable, because, in his words, 'I want to be proud of what I build.'",
+  "It's been an honor having Aiden on our engineering team. From day one, he's demonstrated a strong sense of proactivity, ownership, and initiative.",
 ];
