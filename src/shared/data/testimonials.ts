b5c15466f91c66db9097f437227a6c04b11c4d74
Aiden Threadgoode <a.threadless@proton.me>
2026-08-24 21:49:51 -0400

    fix: adding most quotes and updating to wrap for odd numbers

--- src/shared/data/testimonials.ts
+++ src/shared/data/testimonials.ts
@@ -2,4 +2,6 @@ export const testimonials: string[] = [
   "He is always on the lookout for someone not feeling good about themselves — looking out for the dignity of every person on the team.",
   "Aiden's mindset of continuous improvement shows up in everything he does — he's constantly learning new tech, improving the UX, and making the codebase more maintainable, because, in his words, 'I want to be proud of what I build.'",
   "It's been an honor having Aiden on our engineering team. From day one, he's demonstrated a strong sense of proactivity, ownership, and initiative.",
+  "Working with Aiden means working with someone who brings an incredible work ethic, strong technical ability, and a thoughtful approach. Aiden isn't afraid to speak his mind or tackle difficult problems head on, and somehow manages to make even tough days a lot more fun!",
+  "Aiden is one of the most pleasant, driven, and hardworking people I have had the opportunity to work with. If I were assembling a team to start a new project, Aiden would undoubtedly be one of the engineers I would choose.",
 ];
